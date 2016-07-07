ed60e4794e124bf00ffb06375c4c9b78164cc31d
Nyfinscyf <boucher.kyle@gmail.com>
2016-07-07 18:52:14 +1200

    Changes

--- js/app/homeController.js
+++ js/app/homeController.js
@@ -122,7 +122,7 @@ function($scope, $http, $interval, $filter) {
                 return;
             }
 
-            $scope.AllData = $filter('orderBy')(data, 'date', true);
+            $scope.AllData = $filter('orderBy')(data, 'timeStamp', true);
             //console.log(data);
             // $scope.LightSensor.Data = [{
             //     values:[],
